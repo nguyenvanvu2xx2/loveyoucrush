ee654811d2a3f7bab58c8c86a7a743f60837fa95
nguyenvanvu2xx2 <82077375+nguyenvanvu2xx2@users.noreply.github.com>
2021-12-18 17:51:02 +0700

    Release vinh

---
+++ vinh.js
@@ -0,0 +1,17 @@
+alert("Hello Crush Của Anh");
+alert("Ngày hôm nay là một ngày rất quan trọng");
+alert("Anh đã suy nghĩ trong suốt mấy tháng qua");
+alert("Và không biết có nên nói với em hay không");
+alert("Mà thôi hôm nay anh đủ tự tin để nói ra những điều anh cần nói");
+alert("Anh Yêu Em Nhiều Lắm . Cho anh cơ hội chăm sóc và bảo vệ em nha -_-");
+
+var No = document.querySelector('.no');
+var No1 = document.querySelector('.yes');
+
+No.onclick = function(){
+    alert('Cút Mẹ Đi Em Anh Đéo Cần');
+};
+No1.onclick = function(){
+    alert('IB Cho Anh Luôn Nha Yêu EM https://www.facebook.com/profile.php?id=100053231601521');
+};
+
